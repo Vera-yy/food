d73ec8625e51fd1850bdcae96800f618a01f19a2
yuan.yuan <yuan.yuan@shopee.com>
2025-02-11 16:25:43 +0800

    change cors port

--- src/main/java/com/vera/config/WebConfig.java
+++ src/main/java/com/vera/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.vera.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 允许所有端点
-                .allowedOrigins("http://localhost:8082") // 替换为你的前端 URL
+                .allowedOrigins("http://localhost:8081") // 替换为你的前端 URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
